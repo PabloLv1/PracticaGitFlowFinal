e7d19206660a4a3247a745f91cebe101bc69cca4
PabloLv1 <jose.vasquez@ucrso.info>
2021-12-01 16:19:18 -0600

    Algunos cambios

--- App/Form1.cs
+++ App/Form1.cs
@@ -17,5 +17,33 @@ namespace App
             InitializeComponent();
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] vector = { textBox1.Text.};
+        }
+
+        void ordenamiento(int[] VectorEnviado)
+        {
+            int aux = 0;
+
+            for (int i = 0; i < VectorEnviado.Length - 1; i++)
+            {
+
+                for (int j = 0; i < VectorEnviado.Length - 1; j++)
+                {
+
+                    if (VectorEnviado[j] > VectorEnviado[j + 1])
+                    {
+                        aux = VectorEnviado[j];
+                        VectorEnviado[j] = VectorEnviado[j + 1];
+                        VectorEnviado[j + 1] = aux;
+
+                    }
+
+                }
+
+            }
+        }
+
     }
 }
